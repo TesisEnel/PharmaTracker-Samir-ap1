603bda3676cede1d63d2ede7b27462c528e479a4
Stronauta <samircastillop@gmail.com>
2023-11-07 22:02:31 -0500

    Creación inicial del modelo Clientes

---
+++ PharmaTracker/Shared/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PharmaTracker.Shared
+{
+	internal class Clientes
+	{
+		[Key]
+		public int ClienteId { get; set; }
+		[Required(ErrorMessage = "El nombre es obligatorio")]
+		public string Nombre { get; set; }
+		[Required(ErrorMessage = "La dirección es obligatorio")]
+		public string Direccion { get; set; }
+		[Required(ErrorMessage = "El teléfono es obligatorio")]
+		public string Telefono { get; set; }
+		[Required(ErrorMessage = "El Email es obligatorio")]
+		public string Email { get; set; }
+
+	}
+}
