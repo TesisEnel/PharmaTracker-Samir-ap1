5bc51fc3fcb8a671ad485069a2fe8c1c4ca3226e
Stronauta <samircastillop@gmail.com>
2023-11-10 23:13:47 -0400

    Se instalo el radzen

--- PharmaTracker/Client/Program.cs
+++ PharmaTracker/Client/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PharmaTracker.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddRadzenComponents();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
