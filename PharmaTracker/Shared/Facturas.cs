60be211d8b264f0abed72cee89a1cf9f86ee3340
darcklito <luis_paulino5@ucne.edu.do>
2023-11-10 21:15:38 -0400

    Creacion inicial del modelo de facturas

---
+++ PharmaTracker/Shared/Facturas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PharmaTracker.Shared
+{
+	public class Facturas
+	{
+		[Key]
+		public int FacturaId { get; set; }
+		[Required(ErrorMessage = "El tipo de factura es obligatorio")]
+		public string? TipoFactura { get; set; }
+		[Required(ErrorMessage = "El NCF es obligatorio")]
+		public int NCF { get; set; }
+		[Required(ErrorMessage = "La fecha es obligatorio")]
+		public DateTime Fecha { get; set; }
+		[Required(ErrorMessage = "El cliente es obligatorio")]
+		public int ClienteId { get; set; }
+		[Required(ErrorMessage ="El descuento es requerido (Si no tiene es 0.00)")]
+		public decimal Descuento { get; set; }
+		
+		[Required(ErrorMessage = "El total es obligatorio")]
+		public decimal Total { get; set; }
+	}
+}
