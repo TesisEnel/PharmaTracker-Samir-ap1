a23702eb737a9af3bc9e7762d0f2a1696311044b
darcklito <luis_paulino5@ucne.edu.do>
2023-11-10 21:40:31 -0400

    creacion inicial de modelo para el detalle componente de productos

---
+++ PharmaTracker/Shared/ComponentesProductoD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaTracker.Shared
+{
+	public class ComponentesProductoD
+	{
+		[Key]
+		public int ComponentesProductoId { get; set; }
+		[ForeignKey("ProductoId")]
+		public int ProductoId { get; set; }
+		[Required]
+		public string? Descripción { get; set; }
+	}
+}
