b973cc851e09af2f6c27ee37d243f64a76fef2be
darcklito <luis_paulino5@ucne.edu.do>
2023-11-10 23:23:40 -0400

    errores con radzen solucionados

--- PharmaTracker/Client/Program.cs
+++ PharmaTracker/Client/Program.cs
@@ -7,9 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddRadzenComponents();
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
