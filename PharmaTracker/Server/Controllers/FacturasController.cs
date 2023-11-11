329e512b96dc6a14570470938d70d5671b50da36
Stronauta <samircastillop@gmail.com>
2023-11-10 22:23:20 -0400

    Se creo el controlador de las facturas

---
+++ PharmaTracker/Server/Controllers/FacturasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PharmaTracker.Server.DAL;
+using PharmaTracker.Shared;
+
+namespace PharmaTracker.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturasController : ControllerBase
+    {
+        private readonly PharmaTracketContext _context;
+
+        public FacturasController(PharmaTracketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Facturas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Facturas>>> GetFactura()
+        {
+          if (_context.Factura == null)
+          {
+              return NotFound();
+          }
+            return await _context.Factura.ToListAsync();
+        }
+
+        // GET: api/Facturas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Facturas>> GetFacturas(int id)
+        {
+          if (_context.Factura == null)
+          {
+              return NotFound();
+          }
+            var facturas = await _context.Factura.FindAsync(id);
+
+            if (facturas == null)
+            {
+                return NotFound();
+            }
+
+            return facturas;
+        }
+
+        // PUT: api/Facturas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFacturas(int id, Facturas facturas)
+        {
+            if (id != facturas.FacturaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(facturas).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FacturasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Facturas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Facturas>> PostFacturas(Facturas facturas)
+        {
+          if (_context.Factura == null)
+          {
+              return Problem("Entity set 'PharmaTracketContext.Factura'  is null.");
+          }
+            _context.Factura.Add(facturas);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFacturas", new { id = facturas.FacturaId }, facturas);
+        }
+
+        // DELETE: api/Facturas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFacturas(int id)
+        {
+            if (_context.Factura == null)
+            {
+                return NotFound();
+            }
+            var facturas = await _context.Factura.FindAsync(id);
+            if (facturas == null)
+            {
+                return NotFound();
+            }
+
+            _context.Factura.Remove(facturas);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FacturasExists(int id)
+        {
+            return (_context.Factura?.Any(e => e.FacturaId == id)).GetValueOrDefault();
+        }
+    }
+}
