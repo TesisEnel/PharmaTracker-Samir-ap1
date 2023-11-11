eb8e8ee60999ce8715d116cd5f2604f13b8f49dc
Stronauta <samircastillop@gmail.com>
2023-11-10 22:12:06 -0400

    Se realizo la migracion y se actualizo la base de datos

---
+++ PharmaTracker/Server/Migrations/20231111020839_Inicial.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PharmaTracker.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Dirección = table.Column<string>(type: "TEXT", nullable: false),
+                    Teléfono = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Contraseña = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Factura",
+                columns: table => new
+                {
+                    FacturaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TipoFactura = table.Column<string>(type: "TEXT", nullable: false),
+                    NCF = table.Column<int>(type: "INTEGER", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descuento = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Factura", x => x.FacturaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendedor",
+                columns: table => new
+                {
+                    VendedorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Dirección = table.Column<string>(type: "TEXT", nullable: false),
+                    Teléfono = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Contraseña = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedor", x => x.VendedorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NombreProducto = table.Column<string>(type: "TEXT", nullable: false),
+                    Precio = table.Column<int>(type: "INTEGER", nullable: false),
+                    Laboratorio = table.Column<string>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false),
+                    FacturasFacturaId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                    table.ForeignKey(
+                        name: "FK_Productos_Factura_FacturasFacturaId",
+                        column: x => x.FacturasFacturaId,
+                        principalTable: "Factura",
+                        principalColumn: "FacturaId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComponentesProductoD",
+                columns: table => new
+                {
+                    ComponentesProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descripción = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductosProductoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentesProductoD", x => x.ComponentesProductoId);
+                    table.ForeignKey(
+                        name: "FK_ComponentesProductoD_Productos_ProductosProductoId",
+                        column: x => x.ProductosProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DescripcionProductoD",
+                columns: table => new
+                {
+                    DetalleProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descripción = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductosProductoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DescripcionProductoD", x => x.DetalleProductoId);
+                    table.ForeignKey(
+                        name: "FK_DescripcionProductoD_Productos_ProductosProductoId",
+                        column: x => x.ProductosProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComponentesProductoD_ProductosProductoId",
+                table: "ComponentesProductoD",
+                column: "ProductosProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DescripcionProductoD_ProductosProductoId",
+                table: "DescripcionProductoD",
+                column: "ProductosProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_FacturasFacturaId",
+                table: "Productos",
+                column: "FacturasFacturaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "ComponentesProductoD");
+
+            migrationBuilder.DropTable(
+                name: "DescripcionProductoD");
+
+            migrationBuilder.DropTable(
+                name: "Vendedor");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Factura");
+        }
+    }
+}
