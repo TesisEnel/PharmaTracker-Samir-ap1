9074d49fd3db985736a9c3aae2de4095bb5f1092
darcklito <luis_paulino5@ucne.edu.do>
2023-11-18 20:37:48 -0400

    extension para auntenticar usuario creada

---
+++ PharmaTracker/Client/Extensions/AuthenticacionExtension.cs
@@ -0,0 +1,59 @@
+﻿using PharmaTracker.Shared;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace PharmaTracker.Client.Extensions
+{
+    public class AuthenticacionExtension : AuthenticationStateProvider
+    {
+        private readonly ISessionStorageService _sessionStorage;
+        private ClaimsPrincipal _noInformation = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public AuthenticacionExtension(ISessionStorageService sessionStorage)
+        {
+            _sessionStorage = sessionStorage;
+        }
+
+        public async Task UpdateStatusAuthentication(SesionDTO? userSession)
+        {
+            ClaimsPrincipal claimsPrincipal;
+
+            if (userSession != null)
+            {
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userSession.Nombre),
+                    new Claim(ClaimTypes.Email, userSession.Correo),
+                    new Claim(ClaimTypes.Role, userSession.Rol),
+                },"JwtAuth"));
+
+                await _sessionStorage.SaveStorage("userSession", userSession);
+            }
+            else
+            {
+                claimsPrincipal = _noInformation;
+                await _sessionStorage.RemoveItemAsync("userSession");
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var userSession = await _sessionStorage.GetStorage<SesionDTO>("userSession");
+
+            if (userSession == null)
+                return await Task.FromResult(new AuthenticationState(_noInformation));
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userSession.Nombre),
+                new Claim(ClaimTypes.Email, userSession.Correo),
+                new Claim(ClaimTypes.Role, userSession.Rol),
+            },"JwtAuth"));
+
+            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+        }   
+    }
+}
