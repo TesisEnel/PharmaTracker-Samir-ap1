7e6dd5377ec50ceae3483bb5f5ee3f561fe5e553
darcklito <luis_paulino5@ucne.edu.do>
2023-11-18 20:43:00 -0400

    agregando servicios necesarios para autenticacion al program

--- PharmaTracker/Client/Program.cs
+++ PharmaTracker/Client/Program.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
 using PharmaTracker.Client;
+using PharmaTracker.Client.Extensions;
+using Blazored.SessionStorage;
 using Radzen;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticacionExtension>();
+builder.Services.AddAuthorizationCore();
 
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddRadzenComponents();
