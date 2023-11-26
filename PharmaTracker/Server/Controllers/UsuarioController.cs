b87c469246a032c09d4a593ae7b3c64910fbdd4f
darcklito <luis_paulino5@ucne.edu.do>
2023-11-26 01:56:46 -0400

    modificaciones a la api de usarios para que esta funcione con la base de datos enves de con modelos hardcodeados

--- PharmaTracker/Server/Controllers/UsuarioController.cs
+++ PharmaTracker/Server/Controllers/UsuarioController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PharmaTracker.Shared;
+using PharmaTracker.Server.DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace PharmaTracker.Server.Controllers
 {
@@ -8,33 +10,76 @@ namespace PharmaTracker.Server.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly PharmaTracketContext _context;
+
+        public UsuarioController(PharmaTracketContext context)
+        {
+            _context = context;
+        }
+
         [HttpPost]
         [Route("Login")]
         public async Task<IActionResult> Login([FromBody] LoginDTO login)
         {
             SesionDTO sesionDTO = new SesionDTO();
-            if (login.Correo == "admin@gmail.com" && login.Clave == "admin")
+
+            var adminFound = await _context.Admin.FirstOrDefaultAsync(x => x.Email == login.Correo && x.Contraseña == login.Clave);
+
+            if (adminFound != null && adminFound.Email == login.Correo && adminFound.Contraseña == login.Clave)
+
             {
-                sesionDTO.Nombre = "admin";
+                sesionDTO.Nombre = adminFound!.Nombre!;
                 sesionDTO.Correo = login.Correo;
                 sesionDTO.Rol = "Administrador";
             }
-            else if (login.Correo == "vendedor@gmail.com" && login.Clave == "vendedor")
+
+            /*else if (userFound != null && userFound.Email == login.Correo && userFound.Contraseña != login.Clave)
             {
-                sesionDTO.Nombre = "vendedor";
+                return StatusCode(StatusCodes.Status401Unauthorized, "Contraseña incorrecta");
+            }*/
+
+            var vendedorFound = await _context.Vendedor.FirstOrDefaultAsync(x => x.Email == login.Correo);
+
+            if (vendedorFound != null && vendedorFound.Email == login.Correo && vendedorFound.Contraseña == login.Clave)
+            {
+                sesionDTO.Nombre = vendedorFound.Nombre;
                 sesionDTO.Correo = login.Correo;
                 sesionDTO.Rol = "Vendedor";
             }
-            else if (login.Correo == "cliente@gmail.com" && login.Clave == "cliente")
+
+           /* else if (vendedorFound != null && vendedorFound.Email == login.Correo && vendedorFound.Contraseña != login.Clave)
             {
-                sesionDTO.Nombre = "cliente";
+                StatusCode(StatusCodes.Status401Unauthorized, "Contraseña incorrecta");
+            }*/
+
+            var clienteFound = await _context.Clientes.FirstOrDefaultAsync(x => x.Email == login.Correo && x.Contraseña == login.Clave);
+
+            if (clienteFound != null && clienteFound.Email == login.Correo && clienteFound.Contraseña == login.Clave)
+            {
+                sesionDTO.Nombre = clienteFound.Nombre;
                 sesionDTO.Correo = login.Correo;
                 sesionDTO.Rol = "Cliente";
             }
-            else
+
+            /*else if (clienteFound != null && clienteFound.Email == login.Correo && clienteFound.Contraseña != login.Clave)
             {
-                return BadRequest("Usuario o contraseña incorrecta");
+                return StatusCode(StatusCodes.Status401Unauthorized, "Contraseña incorrecta");
+            }*/
+
+            else if (clienteFound == null || vendedorFound == null || adminFound == null)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "Usuario no encontrado");
             }
+            else if(login.Clave != adminFound.Contraseña || login.Clave != vendedorFound.Contraseña || login.Clave != clienteFound.Contraseña)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "Contraseña no encontrada");
+            }
+
+/*            else if (clienteFound == null && vendedorFound == null && userFound == null)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "Contraseña no encontrada");
+            }*/
+
             return StatusCode(StatusCodes.Status200OK, sesionDTO);
         }
     }
