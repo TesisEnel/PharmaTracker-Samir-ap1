919655da71000fed4f0e935a6dfbc0e85761747e
darcklito <luis_paulino5@ucne.edu.do>
2023-12-01 04:12:04 -0400

    agredando controladores para los tipos de telefonos

---
+++ PharmaTracker/Server/Controllers/AdminTiposTelefonosController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PharmaTracker.Server.DAL;
+using PharmaTracker.Shared;
+
+namespace PharmaTracker.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminTiposTelefonosController : ControllerBase
+    {
+        private readonly PharmaTracketContext _context;
+
+        public AdminTiposTelefonosController(PharmaTracketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AdminTiposTelefonos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdminTiposTelefonos>>> GetAdminTiposTelefonos()
+        {
+          if (_context.AdminTiposTelefonos == null)
+          {
+              return NotFound();
+          }
+            return await _context.AdminTiposTelefonos.ToListAsync();
+        }
+
+        // GET: api/AdminTiposTelefonos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AdminTiposTelefonos>> GetAdminTiposTelefonos(int id)
+        {
+            var tipo = await _context.AdminTiposTelefonos.FindAsync(id);
+
+            return tipo!;
+        }
+    }
+}
---
+++ PharmaTracker/Server/Controllers/VendedorTiposTelefonosController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PharmaTracker.Server.DAL;
+using PharmaTracker.Shared;
+
+namespace PharmaTracker.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendedorTiposTelefonosController : ControllerBase
+    {
+        private readonly PharmaTracketContext _context;
+
+        public VendedorTiposTelefonosController(PharmaTracketContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/VendedorTiposTelefonos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VendedorTiposTelefonos>>> GetVendedorTiposTelefonos()
+        {
+          if (_context.VendedorTiposTelefonos == null)
+          {
+              return NotFound();
+          }
+          return await _context.VendedorTiposTelefonos.ToListAsync();
+        }
+
+        // GET: api/VendedorTiposTelefonos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VendedorTiposTelefonos>> GetVendedorTiposTelefonos(int id)
+        {
+            var tipo = await _context.VendedorTiposTelefonos.FindAsync(id);
+
+            return tipo!;
+        }
+    }
+}
