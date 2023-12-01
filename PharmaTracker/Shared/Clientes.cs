a400a459fab9651e87eac3c3f5b5ee30e85bd736
darcklito <luis_paulino5@ucne.edu.do>
2023-12-01 15:25:20 -0400

    modificacion de regulaciones para clientes

--- PharmaTracker/Shared/Clientes.cs
+++ PharmaTracker/Shared/Clientes.cs
@@ -12,12 +12,15 @@ namespace PharmaTracker.Shared
 		[Key]
 		public int ClienteId { get; set; }
 		[Required(ErrorMessage = "El nombre es obligatorio")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras y espacios")]
 		public string? Nombre { get; set; }
-		[Required(ErrorMessage = "La dirección es obligatorio")]
+		[Required(ErrorMessage = "La dirección es obligatoria")]
 		public string? Dirección { get; set; }
 		[Required(ErrorMessage = "El teléfono es obligatorio")]
+		[RegularExpression(@"^\d{10}$", ErrorMessage = "El teléfono debe tener 10 dígitos")]
 		public string? Teléfono { get; set; }
 		[Required(ErrorMessage = "El Email es obligatorio")]
+		[EmailAddress(ErrorMessage = "La dirección de correo electrónico no es válida")]
 		public string? Email { get; set; }
 		[Required(ErrorMessage = "La contraseña es obligatoria")]
 		public string? Contraseña { get; set; }
