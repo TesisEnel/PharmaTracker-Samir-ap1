6a9cb5fa94bef52261964c862e7fd8a98cd6c826
Stronauta <samircastillop@gmail.com>
2023-12-02 01:20:03 -0400

    Se corrigio errores en el detalle del admin

--- PharmaTracker/Shared/AdminDetalle.cs
+++ PharmaTracker/Shared/AdminDetalle.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PharmaTracker.Shared
 {
-    public class AdminDetalle
-    {
-        [Key]
-        public int AdminDetalleId { get; set; }
-        public int AdminId { get; set; }
-        public int AdminTipoId { get; set; }
-        public string AdminTelefono { get; set; }
-    }
+	public class AdminDetalle
+	{
+		[Key]
+		public int AdminDetalleId { get; set; }
+		[ForeignKey("AdminId")]
+		public int AdminId { get; set; }
+		[Required(ErrorMessage = "El tipo es obligatorio")]
+		public string? AdminTipos { get; set; }
+
+		[Required(ErrorMessage = "El telefono es obligatorio")]
+		[RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "El formato del teléfono debe ser xxx-xxx-xxxx")]
+		public string? AdminTelefono { get; set; }
+	}
 }
+
