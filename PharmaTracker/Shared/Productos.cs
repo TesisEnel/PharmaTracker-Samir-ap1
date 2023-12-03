62412975f1afc24eba01e4a5d49f460f56e68f03
Stronauta <samircastillop@gmail.com>
2023-12-03 15:57:13 -0400

    Migraacion para el detalle

--- PharmaTracker/Shared/Productos.cs
+++ PharmaTracker/Shared/Productos.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PharmaTracker.Shared
 {
@@ -27,7 +28,8 @@ namespace PharmaTracker.Shared
         public string? Unidad { get; set; }
         [Required(ErrorMessage = "La categoría es obligatoria")]
         public string? Categoria { get; set; }
-        
-        public ICollection<DetalleLaboratorioProducto>? detalleLabProducto { get; set; }
-	}
+
+		[ForeignKey("ProductoId")]
+		public ICollection<DetalleLaboratorioProducto> detalleLabProducto { get; set; } = new List<DetalleLaboratorioProducto>();
+	}	
 }
