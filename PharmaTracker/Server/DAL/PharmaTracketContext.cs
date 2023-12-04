6a9d45040b7a2cfee083bf6f7da0ae2495a98ad6
darcklito <luis_paulino5@ucne.edu.do>
2023-12-04 12:37:17 -0400

    agregando modelo para carrito

--- PharmaTracker/Server/DAL/PharmaTracketContext.cs
+++ PharmaTracker/Server/DAL/PharmaTracketContext.cs
@@ -16,7 +16,6 @@ namespace PharmaTracker.Server.DAL
         public DbSet<Facturas> Factura { get; set; }
         public DbSet<Vendedor> Vendedor { get; set; }
         public DbSet<Admin> Admin { get; set; }
-        public DbSet<CarritoCompra> CestaDCompra { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
