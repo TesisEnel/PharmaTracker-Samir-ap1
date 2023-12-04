102e63f881a3f7093b0215a37ae3141a72b65413
darcklito <luis_paulino5@ucne.edu.do>
2023-12-04 12:36:48 -0400

    agregando servicios para funciones con el carrito

---
+++ PharmaTracker/Client/Services/CarritoService/CarritoService.cs
@@ -0,0 +1,70 @@
+﻿using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using PharmaTracker.Client.Services.ProductosService;
+using PharmaTracker.Shared;
+using Radzen;
+
+namespace PharmaTracker.Client.Services.CarritoService
+{
+	public class CarritoService : ICarritoService
+	{
+		private readonly ISessionStorageService _sessionStorageService;
+		private readonly IProductosService _productosService;
+
+		public event Action OnChange;
+        public CarritoService(ISessionStorageService sessionStorageService,
+			IProductosService productosService)
+        {
+			_sessionStorageService = sessionStorageService;
+			_productosService = productosService;
+		}
+
+        public async Task AgregarAlCarrito(Productos producto)
+		{
+			var carrito = await _sessionStorageService.GetItemAsync<List<Productos>>("carrito");
+			if (carrito == null)
+			{
+				carrito = new List<Productos>();
+			}
+			carrito.Add(producto);
+			await _sessionStorageService.SetItemAsync("carrito", carrito);
+
+			OnChange?.Invoke();
+		}
+
+        public async Task<List<ArticulosEnCarrito>> ObtenerArticulosEnCarrito()
+        {
+            var resultado = new List<ArticulosEnCarrito>();
+			var carrito = await _sessionStorageService.GetItemAsync<List<Productos>>("carrito");
+			if (carrito == null)
+				return resultado;
+
+			foreach (var producto in carrito)
+			{
+				var articulo = await _productosService.ObtenerProductos(producto.ProductoId);
+				var articuloEnCarrito = new ArticulosEnCarrito
+				{
+					ProductoId = articulo.ProductoId,
+					NombreProducto = articulo.NombreProducto,
+					Precio = articulo.Precio,
+					Imagen = articulo.Imagen,
+                };
+				resultado.Add(articuloEnCarrito);
+			}
+			return resultado;
+        }
+
+        public async Task EliminarArticulo(ArticulosEnCarrito articulo)
+        {
+            var carrito = await _sessionStorageService.GetItemAsync<List<Productos>>("carrito");
+			if (carrito == null)
+				return;
+			var articuloAEliminar = carrito.Find(x => x.ProductoId == articulo.ProductoId);
+			carrito.Remove(articuloAEliminar);
+
+			await _sessionStorageService.SetItemAsync("carrito", carrito);
+
+			OnChange?.Invoke();
+        }
+    }
+}
---
+++ PharmaTracker/Client/Services/ProductosService/ProductosService.cs
@@ -0,0 +1,28 @@
+﻿using PharmaTracker.Shared;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace PharmaTracker.Client.Services.ProductosService
+{
+	public class ProductosService : IProductosService
+	{
+		private readonly HttpClient _http;
+
+		public event Action OnChange;
+		public List<Productos> Productos { get; set; } = new List<Productos>();
+		public ProductosService(HttpClient http)
+		{
+			_http = http;
+		}
+
+		public async Task CargarProductos()
+		{
+			Productos = await _http.GetFromJsonAsync<List<Productos>>("api/Productos");
+			OnChange?.Invoke();
+		}
+		public async Task<Productos> ObtenerProductos(int id)
+		{
+			return await _http.GetFromJsonAsync<Productos>($"api/Productos/{id}");
+		}
+	}
+}
